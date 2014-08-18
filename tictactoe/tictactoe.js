37475e8e31dabaf25ac945f89318892da5107772
andrew lui <alui@atlassian.com>
2014-08-18 15:58:25 +1000

    Initial commit

---
+++ tictactoe/tictactoe.js
@@ -0,0 +1,14 @@
+/* 
+1. Create a JS object with the grid
+2. Render the grid from the object
+3. Capture click event and add character to grid where user has clicked. Make the character alternate between X and O
+4. Build three functions: 
+	checkRow() which checks if a row is complete, then determines if someone has won
+	checkColumn() which is the same as checkRow, but for a column 
+	checkDiagonal() which is the same as checkRow, but for a diagonal
+5. Call two or three of these functions depending on where character is placed
+*/
+
+$(document).ready(function() {
+
+})
