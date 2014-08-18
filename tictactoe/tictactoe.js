b4d7e4c14a8f38df91b7b936ac7707b29ee062f9
andrew lui <alui@atlassian.com>
2014-08-18 18:08:33 +1000

    Added render functoin

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,7 +1,7 @@
 /* 
 1. Create a JS object with the grid
 2. Render the grid from the object
-3. Capture click event and add character to grid where user has clicked. Make the character alternate between X and O
+3. Capture click event and add character to object and grid where user has clicked. Make the character alternate between X and O
 4. Build three functions: 
 	checkRow() which checks if a row is complete, then determines if someone has won
 	checkColumn() which is the same as checkRow, but for a column 
@@ -9,6 +9,27 @@
 5. Call two or three of these functions depending on where character is placed
 */
 
-$(document).ready(function() {
+var gridObj = {
+	row1 : ["","",""],
+	row2 : ["","",""],
+	row3 : ["","",""]
+}
 
+function render (grid) {
+	var count = 0;
+	for (var row in gridObj) {
+		rowArray = gridObj[row];
+		for (i=0; i<rowArray.length; i++) {
+			divId = '#' + count + i;	
+			console.log(divId+'|'+rowArray[i]);
+			$(divId).text(rowArray[i]);
+		}
+		count++;
+	}
+
+
+}
+
+$(document).ready(function() {
+	render(gridObj);
 })
